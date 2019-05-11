00f98dae4d674ff67cc5f795d4c30a10cf80b892
Sean <sean.pargeter@hotmail.com>
2019-05-10 18:07:20 -0700

    Implemented very basic "talk to NPC" functionality. Press F near the boy to talk to him.

---
+++ Assets/Scripts/DialogueButtons.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueButtons : MonoBehaviour
+{
+    public void Goodbye()
+    {
+        OpenMenu.isPaused = false;
+        Time.timeScale = 1f;
+    }
+}
