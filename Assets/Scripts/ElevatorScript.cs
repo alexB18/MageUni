9b8ea08cf819efa5be2144f083f98c7dd3f4ce1a
Sean <sean.pargeter@hotmail.com>
2019-05-26 20:19:49 -0700

    Created the campus environment, the dorm room, and the skeleton for the fourth level.

---
+++ Assets/Scripts/ElevatorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScript : MonoBehaviour
+{
+    public float top;
+    public float bottom;
+    public float speed;
+
+    private Vector3 moveDirection = Vector3.up;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= top)
+        {
+            moveDirection = Vector3.down;
+        } else if (transform.position.y <= bottom)
+        {
+            moveDirection = Vector3.up;
+        }
+
+        transform.Translate(moveDirection * Time.deltaTime * speed);
+    }
+}
