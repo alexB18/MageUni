8d0368ac6e10d4a2ea34ff4577a0fdfc5b01c696
Abahu <diem.carpe.rs@gmail.com>
2019-05-29 23:40:12 -0700

    Revert "Revert "Implemented Block puzzle in rat level. NOTE: Key still unable to be picked up""
    This reverts commit d2e731e574c4c45e588b8d6b0e546a6e56b6badb.

---
+++ Assets/Scripts/Puzzles/RestrictEnemyInteraction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestrictEnemyInteraction : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Don't allow bad guys to move blocks
+        if (!other.CompareTag("Player"))
+        {
+            Physics.IgnoreCollision(other.GetComponent<Collider>(), this.GetComponent<Collider>());
+        }
+    }
+}
