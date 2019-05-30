eedcdb93435ad4e410be85a2ceece2607edf7440
Abahu <diem.carpe.rs@gmail.com>
2019-05-30 00:17:44 -0700

    Unfucked a few things
    Don't change TImeManager

---
+++ Assets/Scripts/Player/QuestStage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class QuestStage
+{
+    public static int QS = 0;
+    public enum QuestStages
+    {
+        IntroRoommate,
+        IntroLecture,
+        IntroLectureDone,
+    }
+}
--- Assets/Scripts/Puzzles/hidden_key.cs
+++ Assets/Scripts/Puzzles/hidden_key.cs
@@ -10,24 +10,19 @@ public class hidden_key : MonoBehaviour
 
     private void Start()
     {
-        hiddenKey = this.transform.GetChild(0).gameObject;
+        hiddenKey = transform.GetChild(0).gameObject;
         hiddenKey.SetActive(false);
         GetComponentInParent<ColorChanger>().changeColor(Color.red);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(currentActiveTriggers == totalTriggers)
-        {
-            GetComponent<ColorChanger>().changeColor(Color.green);
-            hiddenKey = this.transform.GetChild(0).gameObject;
-            hiddenKey.SetActive(true);
-        }        
-    }
-
     public void setTriggerActive()
     {
-        this.currentActiveTriggers += 1;
+        currentActiveTriggers += 1;
+        if (currentActiveTriggers == totalTriggers)
+        {
+            GetComponent<ColorChanger>().changeColor(Color.green);
+            hiddenKey = transform.GetChild(0).gameObject;
+            hiddenKey.SetActive(true);
+        }
     }
 }
