21070c71e5987e3f448e9708e51bf04094c42602
alexB18 <abrown15@uoregon.edu>
2019-05-31 06:03:50 -0700

    Slime Cave B1 is fully playable. MINUS health/mana pickups/enemies are still jank

---
+++ Assets/Scripts/Puzzles/hidden_door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hidden_door : MonoBehaviour
+{
+    private int currentActiveTriggers = 0;
+    public int totalTriggers = 2;
+    GameObject hiddenDoor;
+
+    // Make sure door is rendered on start
+    private void Start()
+    {
+        hiddenDoor = transform.GetChild(0).gameObject;
+        hiddenDoor.SetActive(true);
+    }
+
+    public void setTriggerActive()
+    {
+        currentActiveTriggers += 1;
+        if (currentActiveTriggers == totalTriggers)
+        {
+            hiddenDoor = transform.GetChild(0).gameObject;
+            hiddenDoor.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzles/hidden_doorInteractDetect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hidden_doorInteractDetect : Interactable
+{
+
+    public override void Interact(GameObject actor)
+    {
+        gameObject.GetComponentInParent<hidden_door>().setTriggerActive();
+    }
+}
