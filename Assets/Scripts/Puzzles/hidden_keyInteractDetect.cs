8e08401318374b11f6cf8ba426c4a7268afae2d3
alexB18 <abrown15@uoregon.edu>
2019-05-31 04:17:18 -0700

    Slime Cave F1 is completely playable. Minus Slime enemies, Health/Mana pickups

---
+++ Assets/Scripts/Puzzles/hidden_keyInteractDetect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hidden_keyInteractDetect : Interactable
+{
+    //private bool inPlace = false;
+    Rigidbody otherRb;
+
+    public override void Interact(GameObject actor)
+    {
+        gameObject.GetComponentInParent<hidden_key>().setTriggerActive();
+    }
+}
