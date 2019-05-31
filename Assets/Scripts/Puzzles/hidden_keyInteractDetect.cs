c6be39611d41d9caeae68bb61cfd8219a241d005
alexB18 <abrown15@uoregon.edu>
2019-05-31 07:25:47 -0700

    Slime Level Fully Playable. Minus Health/Mana Pickups

--- Assets/Scripts/Puzzles/hidden_keyInteractDetect.cs
+++ Assets/Scripts/Puzzles/hidden_keyInteractDetect.cs
@@ -6,9 +6,16 @@ public class hidden_keyInteractDetect : Interactable
 {
     //private bool inPlace = false;
     Rigidbody otherRb;
+    private bool executed = false;
 
     public override void Interact(GameObject actor)
     {
-        gameObject.GetComponentInParent<hidden_key>().setTriggerActive();
+        if(!executed)
+        {
+            gameObject.GetComponentInParent<hidden_key>().setTriggerActive();
+            executed = true;
+        }
+        
+
     }
 }
